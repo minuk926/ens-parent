3404a569c02143b94cbe1231a95e04ee1dd3b5f0
Jonguk. Lim <minuk926@gmail.com>
2023-05-02 00:38:32 +0900

    feat: ens-front add

---
+++ ens-front/src/main/java/kr/xit/EnsFrontApplication.java
@@ -0,0 +1,13 @@
+package kr.xit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsFrontApplication.class, args);
+    }
+
+}
---
+++ ens-front/src/test/java/kr/xit/EnsFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.xit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
